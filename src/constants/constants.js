45cff9c07bb9cee103813a2aa9c3f46261bdfa2b
rutul089 <rutul089@gmail.com>
2024-03-21 00:47:40 +0530

    Added Onboarding screen

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,32 @@
+import images from '../assets/images';
+
 export const AppConstant = {
   chatType: 1,
   statusType: 2,
   tokenType: 1,
   swapType: 2,
 };
+
+export const OnBoardItems = [
+  {
+    id: '0',
+    title: 'Crypto Wallet',
+    subtitle:
+      'Securely manage your digital assets send, and receive cryptocurrencies ',
+    image: images.onboarding_1,
+  },
+  {
+    id: '1',
+    title: 'Chat & Status',
+    subtitle:
+      'Securely manage your digital assets send, and receive cryptocurrencies',
+    image: images.onboarding_2,
+  },
+  {
+    id: '2',
+    title: 'Chat & Status',
+    subtitle:
+      'Securely manage your digital assets send, and receive cryptocurrencies',
+    image: images.onboarding_3,
+  },
+];
--- src/screens/WelcomeScreen/Welcome_Component.js
+++ src/screens/WelcomeScreen/Welcome_Component.js
@@ -1,22 +1,101 @@
 import {useTheme} from '@react-navigation/native';
 import React from 'react';
-import {Image, View} from 'react-native';
+import {Dimensions, Image, View} from 'react-native';
 import images from '../../assets/images';
 import {Button, Spacing, Text} from '../../components';
 import Strings from '../../localization/Strings';
 import theme from '../../theme';
+import AppIntroSlider from '../../components/AppIntroSlider';
+import {OnBoardItems} from '../../constants/constants';
+const {width, height} = Dimensions.get('window');
 
 const Welcome_Component = ({button1Press, button2Press}) => {
   const {colors} = useTheme();
+
+  const _renderNextButton = () => {
+    return (
+      <View>
+        <Button
+          label={Strings.createNewWallet}
+          onPress={button1Press}
+          showIconRight={true}
+        />
+      </View>
+    );
+  };
+
+  const _renderDoneButton = () => {
+    return (
+      <>
+        <Button
+          label={Strings.importWallet}
+          variant="ghost"
+          onPress={button2Press}
+          themedColor={colors.primaryMainColor}
+        />
+      </>
+    );
+  };
+
+  const Slide = ({item}) => {
+    return (
+      <View style={{alignItems: 'center', width: width, flex: 1}}>
+        <Image
+          source={item?.image}
+          style={{height: '55%', width, resizeMode: 'contain'}}
+        />
+        <View
+          style={{
+            marginTop: theme.normalize(-22),
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '72%',
+            // paddingHorizontal: theme.normalize(30),
+          }}>
+          <Text type={'large-header'} interBold={true}>
+            {item?.title}
+          </Text>
+          <Spacing />
+          <Text
+            color={colors?.grayLight}
+            textAlign={'center'}
+            type={'helper-text'}
+            size={'smm'}
+            lineHeight={theme.typography.lineHeights.lg}>
+            {item?.subtitle}
+          </Text>
+        </View>
+      </View>
+    );
+  };
 
   return (
     <View
       style={{
         flex: 1,
         backgroundColor: colors.primaryBg,
-        paddingHorizontal: theme.sizes.spacing.ph,
+        // paddingHorizontal: theme.sizes.spacing.ph,
       }}>
-      <View style={{flex: 3, alignItems: 'center'}}>
+      <Spacing size={theme.sizes.spacing.xl2} />
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <Text type={'inter-medium'} size={theme.typography.fontSizes.xl}>
+          {Strings.welcome}
+        </Text>
+        <Text type={'helper-text'} color={colors.caption}>
+          {Strings.welcomeNote}
+        </Text>
+      </View>
+      <AppIntroSlider
+        renderItem={({item}) => <Slide item={item} />}
+        data={OnBoardItems}
+        bottomButton
+        showSkipButton
+        renderNextButton={_renderNextButton}
+        renderDoneButton={_renderDoneButton}
+        activeDotStyle={{backgroundColor: colors?.primaryMainColor}}
+        dotStyle={{backgroundColor: colors?.gray1}}
+      />
+      {/* <View style={{flex: 3, alignItems: 'center'}}>
         <Spacing size={theme.sizes.spacing.xl2} />
         <Text type={'inter-medium'} size={theme.typography.fontSizes.xl}>
           {Strings.welcome}
@@ -48,7 +127,7 @@ const Welcome_Component = ({button1Press, button2Press}) => {
           onPress={button2Press}
           themedColor={colors.primaryMainColor}
         />
-      </View>
+      </View> */}
     </View>
   );
 };
