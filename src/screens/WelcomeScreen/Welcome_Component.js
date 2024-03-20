010ef5af6b0935f86981b53168fd7b84b37359d2
rutul089 <rutul089@gmail.com>
2024-03-21 00:50:46 +0530

    Onboarding Screen

--- src/screens/WelcomeScreen/Welcome_Component.js
+++ src/screens/WelcomeScreen/Welcome_Component.js
@@ -50,7 +50,6 @@ const Welcome_Component = ({button1Press, button2Press}) => {
             justifyContent: 'center',
             alignItems: 'center',
             width: '72%',
-            // paddingHorizontal: theme.normalize(30),
           }}>
           <Text type={'large-header'} interBold={true}>
             {item?.title}
@@ -74,7 +73,6 @@ const Welcome_Component = ({button1Press, button2Press}) => {
       style={{
         flex: 1,
         backgroundColor: colors.primaryBg,
-        // paddingHorizontal: theme.sizes.spacing.ph,
       }}>
       <Spacing size={theme.sizes.spacing.xl2} />
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
@@ -95,39 +93,6 @@ const Welcome_Component = ({button1Press, button2Press}) => {
         activeDotStyle={{backgroundColor: colors?.primaryMainColor}}
         dotStyle={{backgroundColor: colors?.gray1}}
       />
-      {/* <View style={{flex: 3, alignItems: 'center'}}>
-        <Spacing size={theme.sizes.spacing.xl2} />
-        <Text type={'inter-medium'} size={theme.typography.fontSizes.xl}>
-          {Strings.welcome}
-        </Text>
-        <Text type={'helper-text'} color={colors.caption}>
-          {Strings.welcomeNote}
-        </Text>
-        <Spacing size={theme.normalize(90)} />
-        <Image
-          source={images.ic_wallet_image}
-          resizeMode="contain"
-          style={{
-            width: theme.normalize(280),
-            height: theme.normalize(280),
-            top: 15,
-          }}
-        />
-      </View>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-        }}>
-        <Button label={Strings.createNewWallet} onPress={button1Press} />
-        <Spacing />
-        <Button
-          label={Strings.importWallet}
-          variant="ghost"
-          onPress={button2Press}
-          themedColor={colors.primaryMainColor}
-        />
-      </View> */}
     </View>
   );
 };
